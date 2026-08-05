7bc37e6f9be7a7a1666d9b7602f71fa8aa48e8f4
yosel <yosel11517@gmail.com>
2026-08-05 17:25:46 -0600

    The UI layout for the AddClient screen is implemented.

--- app/src/main/java/yosel/dev/atti/screens/add_client/ui/AddClienteComp.kt
+++ app/src/main/java/yosel/dev/atti/screens/add_client/ui/AddClienteComp.kt
@@ -1,7 +1,29 @@
 package yosel.dev.atti.screens.add_client.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.PersonAdd
+import androidx.compose.material3.Button
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import yosel.dev.atti.ui.theme.AttiTheme
 
 @Composable
 fun BodyAddClient(
@@ -9,5 +31,63 @@ fun BodyAddClient(
     state: AddClientState,
     onAction: (AddClientAction) -> Unit
 ) {
+    Column(
+        modifier = modifier
+    ) {
+        ClientForm(
+            modifier = Modifier.weight(1f)
+        )
 
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {},
+            enabled = state.formState.isValid
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.PersonAdd,
+                    contentDescription = "registrar cliente"
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = "Registrar cliente")
+            }
+        }
+    }
+}
+
+@Composable
+private fun ClientForm(modifier: Modifier) {
+    LazyColumn(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        item {
+            Text(
+                text = "Complete los datos para dar de alta a un nuevo cliente.",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun BodyPreview() {
+    AttiTheme {
+        BodyAddClient(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+                .padding(24.dp),
+            state = AddClientState(),
+            onAction = {}
+        )
+    }
 }
