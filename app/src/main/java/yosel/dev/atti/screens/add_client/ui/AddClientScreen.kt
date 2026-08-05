94f2aca540c66360bed9ba3f1f50599bbddad9c0
yosel <yosel11517@gmail.com>
2026-08-05 17:10:24 -0600

    The AddClientScreen is updated and a LoadingDialog component is added.

--- app/src/main/java/yosel/dev/atti/screens/add_client/ui/AddClientScreen.kt
+++ app/src/main/java/yosel/dev/atti/screens/add_client/ui/AddClientScreen.kt
@@ -1,9 +1,20 @@
 package yosel.dev.atti.screens.add_client.ui
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import yosel.dev.atti.core.components.CustomSnackbarHost
+import yosel.dev.atti.core.components.LoadingDialog
+import yosel.dev.atti.core.components.TopBarGlobal
 
 @Composable
 fun AddClientScreen(
@@ -16,7 +27,36 @@ fun AddClientScreen(
     Scaffold(
         modifier = modifier,
         snackbarHost = {
-
+            CustomSnackbarHost(hostState = snackBarHostState)
+        },
+        topBar = {
+            TopBarGlobal(
+                title = "Registrar Cliente",
+                onBack = onBack
+            )
         }
-    ) { paddingValues ->  }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .consumeWindowInsets(paddingValues)
+                .imePadding()
+        ){
+            BodyAddClient(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 24.dp),
+                state = state,
+                onAction = onAction
+            )
+        }
+
+        if (state.isLoadingAddClient){
+            LoadingDialog(
+                title = "Registrando Cliente...",
+                colorTitle = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
 }
---
+++ app/src/main/java/yosel/dev/atti/screens/add_client/ui/AddClienteComp.kt
@@ -0,0 +1,13 @@
+package yosel.dev.atti.screens.add_client.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BodyAddClient(
+    modifier: Modifier = Modifier,
+    state: AddClientState,
+    onAction: (AddClientAction) -> Unit
+) {
+
+}
