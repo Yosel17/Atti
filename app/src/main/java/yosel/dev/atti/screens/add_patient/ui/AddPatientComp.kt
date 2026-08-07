559af875acfedbf7f2043a845bc706518992693e
yosel <yosel11517@gmail.com>
2026-08-07 15:26:38 -0600

    The AddPatient screen is updated with the body component and catalog loading logic.

---
+++ app/src/main/java/yosel/dev/atti/screens/add_patient/ui/AddPatientComp.kt
@@ -0,0 +1,18 @@
+package yosel.dev.atti.screens.add_patient.ui
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BodyAddPatient(
+    modifier: Modifier = Modifier,
+    state: AddPatientState,
+    onAction: (AddPatientAction) -> Unit
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+
+    }
+}
--- app/src/main/java/yosel/dev/atti/screens/add_patient/ui/AddPatientScreen.kt
+++ app/src/main/java/yosel/dev/atti/screens/add_patient/ui/AddPatientScreen.kt
@@ -1,6 +1,7 @@
 package yosel.dev.atti.screens.add_patient.ui
 
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,18 +9,21 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Category
+import androidx.compose.material.icons.automirrored.outlined.ListAlt
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.LoadingIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import yosel.dev.atti.core.components.CustomSnackbarHost
 import yosel.dev.atti.core.components.EmptyGlobal
 import yosel.dev.atti.core.components.TopBarGlobal
+import yosel.dev.atti.ui.theme.AttiTheme
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
@@ -72,14 +76,39 @@ fun AddPatientScreen(
                     targetState.speciesCatalog.isEmpty() && targetState.genderCatalog.isEmpty() -> {
                         EmptyGlobal(
                             title = "No se pudieron cargar los catálogos",
-                            subTitle = "No es posible registrar clientes sin los catálogos. Inténtalo de nuevo.",
-                            icon = Icons.Outlined.Category,
+                            subTitle = "No es posible registrar pacientes sin los catálogos. Inténtalo de nuevo.",
+                            icon = Icons.AutoMirrored.Outlined.ListAlt,
                             showAction = true,
                             onClickAction = { onAction(AddPatientAction.TryCatalogsAgain) }
+                        )
+                    }
+                    else -> {
+                        BodyAddPatient(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(horizontal = 24.dp),
+                            state = state,
+                            onAction = onAction
                         )
                     }
                 }
             }
         }
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun ScreenPreview() {
+    AttiTheme {
+        AddPatientScreen(
+            modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background),
+            state = AddPatientState(
+                isLoadingCatalogs = false
+            ),
+            snackBarHostState = SnackbarHostState(),
+            onAction = {},
+            onBack = {}
+        )
     }
 }
