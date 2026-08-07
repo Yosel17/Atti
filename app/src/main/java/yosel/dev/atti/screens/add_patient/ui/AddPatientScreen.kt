86d0a745803e0bef592baccf5353664aa30e3188
yosel <yosel11517@gmail.com>
2026-08-07 14:59:02 -0600

    The AddPatientScreen UI component is added.

---
+++ app/src/main/java/yosel/dev/atti/screens/add_patient/ui/AddPatientScreen.kt
@@ -0,0 +1,76 @@
+package yosel.dev.atti.screens.add_patient.ui
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.LoadingIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import yosel.dev.atti.core.components.CustomSnackbarHost
+import yosel.dev.atti.core.components.TopBarGlobal
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+fun AddPatientScreen(
+    modifier: Modifier = Modifier,
+    state: AddPatientState,
+    snackBarHostState: SnackbarHostState,
+    onAction: (AddPatientAction) -> Unit,
+    onBack: () -> Unit
+) {
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = {
+            CustomSnackbarHost(hostState = snackBarHostState)
+        },
+        topBar = {
+            TopBarGlobal(
+                title = "Registrar Paciente",
+                onBack = onBack
+            )
+        }
+    ){ paddingValues ->
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .consumeWindowInsets(paddingValues)
+                .imePadding()
+        ){
+            AnimatedContent(
+                targetState = state,
+                contentKey = { targetState ->
+                    when{
+                        targetState.isLoadingCatalogs -> "LOADING"
+                        targetState.speciesCatalog.isEmpty() && targetState.genderCatalog.isEmpty() -> "EMPTY"
+                        else -> "CONTENT"
+                    }
+                },
+                label = "AddPatientScreenAnimation"
+            ) { targetState ->
+                when {
+                    targetState.isLoadingCatalogs -> {
+                        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                            LoadingIndicator(
+                                modifier = Modifier.size(75.dp)
+                            )
+                        }
+                    }
+                    targetState.speciesCatalog.isEmpty() && targetState.genderCatalog.isEmpty() -> {
+
+                    }
+                }
+            }
+        }
+    }
+}
