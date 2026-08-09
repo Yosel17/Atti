86ed26c16587e859c556bee6162db7ee059cb211
yosel <yosel11517@gmail.com>
2026-08-09 15:45:25 -0600

    The DetailPatientScreen is updated to handle loading and empty states, and the BodyDetailPatient component is added.

--- app/src/main/java/yosel/dev/atti/screens/detail_patient/ui/DetailPatientScreen.kt
+++ app/src/main/java/yosel/dev/atti/screens/detail_patient/ui/DetailPatientScreen.kt
@@ -1,13 +1,20 @@
 package yosel.dev.atti.screens.detail_patient.ui
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.material.icons.rounded.Edit
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LoadingIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
@@ -16,8 +23,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import yosel.dev.atti.core.components.CustomSnackbarHost
+import yosel.dev.atti.core.components.EmptyGlobal
 import yosel.dev.atti.core.components.TopBarGlobal
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun DetailPatientScreen(
     modifier: Modifier = Modifier,
@@ -61,11 +70,52 @@ fun DetailPatientScreen(
                                     tint = MaterialTheme.colorScheme.errorContainer
                                 )
                             }
-
                         }
                     }
                 }
             )
         }
-    ) { }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ){
+            AnimatedContent(
+                targetState = state,
+                contentKey = { targetState ->
+                    when{
+                        targetState.isLoading -> "LOADING"
+                        targetState.patient.id.isEmpty() -> "EMPTY"
+                        else -> "CONTENT"
+                    }
+                },
+                label = "DetailPatientScreenAnimation"
+            ){ targetState ->
+                when{
+                    targetState.isLoading ->{
+                        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                            LoadingIndicator(
+                                modifier = Modifier.size(75.dp)
+                            )
+                        }
+                    }
+                    targetState.patient.id.isEmpty() ->{
+                        EmptyGlobal(
+                            title = "No se pudo encontrar al paciente",
+                            subTitle = "Intenta de nuevo más tarde"
+                        )
+                    }
+                    else ->{
+                        BodyDetailPatient(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(horizontal = 24.dp),
+                            state = state,
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
