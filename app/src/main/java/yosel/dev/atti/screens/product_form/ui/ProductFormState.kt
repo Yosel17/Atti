2010b955ae01b91fd2ec0edcbcecfcdff549c671
yosel <yosel11517@gmail.com>
2026-08-13 14:27:32 -0600

    Supplier data fetching is added to the product form.

--- app/src/main/java/yosel/dev/atti/screens/product_form/ui/ProductFormState.kt
+++ app/src/main/java/yosel/dev/atti/screens/product_form/ui/ProductFormState.kt
@@ -1,10 +1,14 @@
 package yosel.dev.atti.screens.product_form.ui
 
 import yosel.dev.atti.core.models.model.AppCatalogModel
+import yosel.dev.atti.core.models.model.SupplierModel
 
 data class ProductFormState(
     val isLoadingDataInitial: Boolean = true,
     val productCategoryCatalog: List<AppCatalogModel> = emptyList(),
     val productUnitOfMeasureCatalog: List<AppCatalogModel> = emptyList(),
-    val isSuccessGetCategory: Boolean = false
+    val isSuccessGetCategory: Boolean = false,
+    val suppliers: List<SupplierModel> = emptyList(),
+    val filteredSuppliers: List<SupplierModel> = emptyList(),
+    val isSuccessGetSuppliers: Boolean = false,
 )
