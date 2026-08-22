a80c912d6e6ce71ff4891c9b4660fb12e0732c6b
yosel <yosel11517@gmail.com>
2026-08-22 16:47:37 -0600

    The screen skeleton is added.

---
+++ app/src/main/java/yosel/dev/atti/screens/detail_consultation/ui/DetailConsultationComp.kt
@@ -0,0 +1,17 @@
+package yosel.dev.atti.screens.detail_consultation.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BodyDetailConsultation(
+    modifier: Modifier = Modifier,
+    state: DetailConsultationState
+) {
+    Column(
+        modifier = modifier
+    ) {
+
+    }
+}
