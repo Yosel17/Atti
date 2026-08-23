54853fd0c42b6281e980f92eac732993165bbc27
yosel <yosel11517@gmail.com>
2026-08-22 20:01:12 -0600

    The function is added to the repository.

--- app/src/main/java/yosel/dev/atti/screens/detail_consultation/domain/DetailConsultationRepository.kt
+++ app/src/main/java/yosel/dev/atti/screens/detail_consultation/domain/DetailConsultationRepository.kt
@@ -2,11 +2,12 @@ package yosel.dev.atti.screens.detail_consultation.domain
 
 import kotlinx.coroutines.flow.Flow
 import yosel.dev.atti.core.models.model.AppCatalogModel
+import yosel.dev.atti.core.models.model.ConsultationTypeStepWithDetailsModel
 import yosel.dev.atti.core.models.model.ConsultationWithDetailsModel
 
 interface DetailConsultationRepository {
 
     fun getConsultationWithDetailsFlow(consultationId: String): Flow<ConsultationWithDetailsModel?>
 
-    suspend fun getConsultationSteps(): Result<List<AppCatalogModel>>
+    suspend fun getConsultationSteps(): Result<List<ConsultationTypeStepWithDetailsModel>>
 }
