7eb011560d62505eb48d087df833f251401ce285
yosel <yosel11517@gmail.com>
2026-09-11 15:57:04 -0600

    ExifInterface migration to AndroidX. The androidx-exifinterface dependency is added to the version catalog and app build file. The ExifInterface import is updated to AndroidX in ConsentFormRepositoryImpl.

--- app/src/main/java/yosel/dev/atti/screens/consent_form/data/ConsentFormRepositoryImpl.kt
+++ app/src/main/java/yosel/dev/atti/screens/consent_form/data/ConsentFormRepositoryImpl.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Matrix
-import android.media.ExifInterface
+import androidx.exifinterface.media.ExifInterface
 import android.net.Uri
 import androidx.core.graphics.scale
 import androidx.room.withTransaction
